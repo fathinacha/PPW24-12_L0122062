edae15062ae5c3ede5ffa73e9daddaa99de27116
fathinacha <fathin.acha456@gmail.com>
2024-06-17 23:13:31 +0700

    PPW24-12_L0122062 upload

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>List Buku Perpustakaan</title>
+</head>
+<body>
+    <header>
+        <h1>List Buku Perpustakaan</h1>
+    </header>
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>
